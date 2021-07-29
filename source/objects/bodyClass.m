0cdbd9bd431a0bbb5d3358ee9549154b8ceeb712
akeeste <akeeste@sandia.gov>
2021-07-29 09:32:06 -0600

    restore mass adjustments

--- source/objects/bodyClass.m
+++ source/objects/bodyClass.m
@@ -285,27 +285,25 @@ classdef bodyClass<handle
             if B2B == 1
                 tmp.fadm=diag(obj.hydroForce.fAddedMass(:,1+(iBod-1)*6:6+(iBod-1)*6));
                 tmp.adjmass = mean(tmp.fadm(1:3))*adjMassWeightFun;
-                tmp.adjinertia = tmp.fadm(4:6)*adjMassWeightFun;
                 obj.mass = obj.mass + tmp.adjmass;
                 obj.momOfInertia = obj.momOfInertia+tmp.fadm(4:6)';
                 obj.hydroForce.fAddedMass(1,1+(iBod-1)*6) = obj.hydroForce.fAddedMass(1,1+(iBod-1)*6) - tmp.adjmass;
                 obj.hydroForce.fAddedMass(2,2+(iBod-1)*6) = obj.hydroForce.fAddedMass(2,2+(iBod-1)*6) - tmp.adjmass;
                 obj.hydroForce.fAddedMass(3,3+(iBod-1)*6) = obj.hydroForce.fAddedMass(3,3+(iBod-1)*6) - tmp.adjmass;
-                obj.hydroForce.fAddedMass(4,4+(iBod-1)*6) = obj.hydroForce.fAddedMass(4,4+(iBod-1)*6) - tmp.adjinertia(1);
-                obj.hydroForce.fAddedMass(5,5+(iBod-1)*6) = obj.hydroForce.fAddedMass(5,5+(iBod-1)*6) - tmp.adjinertia(1);
-                obj.hydroForce.fAddedMass(6,6+(iBod-1)*6) = obj.hydroForce.fAddedMass(6,6+(iBod-1)*6) - tmp.adjinertia(1);
+                obj.hydroForce.fAddedMass(4,4+(iBod-1)*6) = 0;
+                obj.hydroForce.fAddedMass(5,5+(iBod-1)*6) = 0;
+                obj.hydroForce.fAddedMass(6,6+(iBod-1)*6) = 0;
             else
                 tmp.fadm=diag(obj.hydroForce.fAddedMass);
                 tmp.adjmass = mean(tmp.fadm(1:3))*adjMassWeightFun;
-                tmp.adjinertia = tmp.fadm(4:6)*adjMassWeightFun;
                 obj.mass = obj.mass + tmp.adjmass;
-                obj.momOfInertia = obj.momOfInertia + tmp.adjinertia';
+                obj.momOfInertia = obj.momOfInertia+tmp.fadm(4:6)';
                 obj.hydroForce.fAddedMass(1,1) = obj.hydroForce.fAddedMass(1,1) - tmp.adjmass;
                 obj.hydroForce.fAddedMass(2,2) = obj.hydroForce.fAddedMass(2,2) - tmp.adjmass;
                 obj.hydroForce.fAddedMass(3,3) = obj.hydroForce.fAddedMass(3,3) - tmp.adjmass;
-                obj.hydroForce.fAddedMass(4,4) = obj.hydroForce.fAddedMass(4,4) - tmp.adjinertia(1);
-                obj.hydroForce.fAddedMass(5,5) = obj.hydroForce.fAddedMass(5,5) - tmp.adjinertia(2);
-                obj.hydroForce.fAddedMass(6,6) = obj.hydroForce.fAddedMass(6,6) - tmp.adjinertia(3);
+                obj.hydroForce.fAddedMass(4,4) = 0;
+                obj.hydroForce.fAddedMass(5,5) = 0;
+                obj.hydroForce.fAddedMass(6,6) = 0;
             end
         end
         
