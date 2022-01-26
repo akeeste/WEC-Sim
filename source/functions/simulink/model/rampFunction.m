ad9e7654a84eccb4a50c8938de8db1daba8a222b
akeeste <akeeste@sandia.gov>
2022-01-26 16:09:31 -0700

    ramp function description

--- source/functions/simulink/model/rampFunction.m
+++ source/functions/simulink/model/rampFunction.m
@@ -1,4 +1,6 @@
 function ramp = rampFunction(time,rampTime)
+    % This function calculates the cosine ramp function with a given time
+    % series and ramp time.
     ramp = (1 + cos(pi + pi*time/rampTime))/2;
     ramp(time>rampTime) = 1;
 end
