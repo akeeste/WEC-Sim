3321054518197de4128045d4ca34939e89eb9a91
akeeste <akeeste@sandia.gov>
2022-01-26 16:04:30 -0700

    function argument validation for waveElevation.m

--- source/functions/simulink/model/waveElevation.m
+++ source/functions/simulink/model/waveElevation.m
@@ -1,25 +1,29 @@
 function elevation = waveElevation(A,w,dw,time,k,x,y,waveDir,waveSpread,phaseRand,typeNum)
 % Calculates the wave elevation at a point in space with given wave properties. 
 % 
-% Note: cannot input multiple time steps and multiple locations at once.
-% i.e. Simulink nonlinear wave elevation inputs multiple locations (stl
-% centers) while the pre/post-processing input multiple times at a single
-% wave gauge location
+% Note: cannot currently take multiple time steps and multiple locations at
+% once. e.g. Simulink nonlinear wave elevation inputs multiple locations
+% (stl centers) while the pre/post-processing input multiple times at a
+% single wave gauge location
 
-% Reshape variables so that their dimensions work together. New dimensions
-% should be:
+arguments
+    A (1,1,:) double {mustBeNonnegative}
+    w (1,1,:) double {mustBeNonnegative}
+    dw (1,1,:) double {mustBeNonnegative}
+    time (:,1,1) double {mustBeNonnegative}
+    k (1,1,:) double {mustBeNonnegative}
+    x (:,1,1) double {mustBeReal isfinite}
+    y (:,1,1) double {mustBeReal isfinite}
+    waveDir (1,:,1) double {mustBeReal isfinite}
+    waveSpread (1,:,1) double {mustBeNonnegative}
+    phaseRand (1,:,:) double {mustBeReal isfinite}
+    typeNum (1,1) double {mustBeNonnegative}
+end
+
+% Argument validation reshapes variables so that their dimensions work 
+% together. New dimensions should be:
 % [nXPoints, nDirections, nFrequencies] or [nTimesteps, nDirections, nFrequencies];
 totalSize = ones(length(x), length(waveDir), length(A));
-x = reshape(x,[length(x) 1 1]);
-y = reshape(y,[length(y) 1 1]);
-time = reshape(time,[length(time) 1 1]);
-A = reshape(A,[1 1 length(A)]);
-w = reshape(w,[1 1 length(w)]);
-dw = reshape(dw,[1 1 length(dw)]);
-k = reshape(k,[1 1 length(k)]);
-phaseRand = reshape(phaseRand,[1 length(waveDir) length(A)]);
-waveDir = reshape(waveDir,[1 length(waveDir) 1]);
-waveSpread = reshape(waveSpread,[1 length(waveSpread) 1]);
 
 % Calculate the wave elevation for each wave type
 X = x.*cos(waveDir*pi/180) + y.*sin(waveDir*pi/180);
@@ -31,6 +35,6 @@ elseif typeNum <30
     temp = sqrt(A.*dw).*waveSpread.*cos(w.*time - k.*X + phaseRand);
 end
 elevation = zeros(length(x),1);
-elevation = sum(temp,[2 3]);
+elevation = sum(temp,[2 3]); % sum across all directions (2) and frequencies (3)
 
 end
